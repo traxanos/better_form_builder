759bd53a7a46d3f67e8200cade9bea5fff39d882
Marco Scholl <develop@marco-scholl.de>
2009-07-22 23:44:45 +0200

    do not display attribute name for base

--- lib/better_form_builder.rb
+++ lib/better_form_builder.rb
@@ -6,7 +6,15 @@ module ActiveRecord
       errors = @errors[attribute.to_s]
       return nil if errors.nil?
       
-      errors.collect { |error| (error.at(0) == '^') ? error.slice(1..-1) : "#{@base.class.human_attribute_name(attribute.to_s)} #{error}" }
+      errors.collect do |error|
+        if error.at(0) == '^'
+          error.slice(1..-1)
+        elsif attribute.to_s == 'base'
+          error
+        else
+          "#{@base.class.human_attribute_name(attribute.to_s)} #{error}"
+        end
+      end
     end
   end
 end
